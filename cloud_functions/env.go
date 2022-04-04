8b97655e4f3d2762b110f88a66d35c48965d890c
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-04-04 12:08:39 -0400

    add cloud functions

---
+++ cloud_functions/env.go
@@ -0,0 +1,14 @@
+package cloud_functions
+
+import (
+	"fmt"
+	"os"
+)
+
+func LoadEnvVarOrPanic(envVarName string) string {
+	val, hasVal := os.LookupEnv(envVarName)
+	if !hasVal {
+		panic(fmt.Sprintf("Env variabel %s not found", envVarName))
+	}
+	return val
+}
